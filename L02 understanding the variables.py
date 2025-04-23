77ebc46658ce21d19b30e1f67e1514ca559e1d25
Prog-Smiler <progsmiler@gmail.com>
2025-04-23 21:13:23 +0545

    L02 variable

---
+++ L02 understanding the variables.py
@@ -0,0 +1,14 @@
+x = 5
+y = "John"
+z = 7.3
+
+print(x)
+print(y)
+print(z)
+
+codingal = "codingal"
+
+name = input("Enter your name: ")
+print("\nHello " + name, "\nWelcome to codingal")
+
+age = int(input("How old are you? "))
