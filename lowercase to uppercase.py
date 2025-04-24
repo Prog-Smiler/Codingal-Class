daf4532852ca993f5ad5c434a26c0eb934751c13
Prog-Smiler <progsmiler@gmail.com>
2025-04-24 20:39:12 +0545

    Lowercase to Uppercase

---
+++ lowercase to uppercase.py
@@ -0,0 +1,12 @@
+RamsMsg = "congratulations"
+
+print("Original: ")
+print(RamsMsg)
+
+print("\nAfter Coversion: (only the first character)")
+RamsMsg = RamsMsg.capitalize()
+print(RamsMsg)
+
+print("\nAfter Coversion: (Whole word)")
+RamsMsg = RamsMsg.upper()
+print(RamsMsg)
---
+++ numberswaping.py
@@ -0,0 +1,19 @@
+x = 7
+y = 10
+z = 1
+
+print("Original: ")
+print(x)
+print(y)
+print(z)
+
+a = x
+x = y
+y = z
+z = a
+
+
+print("After Swapping: ")
+print(x)
+print(y)
+print(z)
