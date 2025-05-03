55835d5aa7a1c2d1270702fb3b64b8bbbda535c3
Prog-Smiler <progsmiler@gmail.com>
2025-05-03 21:51:26 +0545

    Square root

---
+++ square.py
@@ -0,0 +1,11 @@
+#importing required library
+import math
+
+#Getting the number from the user
+number = int(input("Enter the number: "))
+
+#Finding the square of the number
+square_root = math.pow(number, 2)
+
+#Displaying the output to the user: 
+print(f"The Square root of {number} is {square_root}")
