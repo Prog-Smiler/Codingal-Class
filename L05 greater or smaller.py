551379b4c702da5bc436c4fed490aa07799dafcb
Prog-Smiler <progsmiler@gmail.com>
2025-05-08 20:17:58 +0545

    L05

---
+++ L05 greater or smaller.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the number: "))
+
+if n < 15:
+    print(f"{n} is smaller than 15")
+    print("I am in IF block")
+
+else:
+    print(f"{n} is greater than 15")
+    print("I am in Else Block")
+
+print("I am not in if and else blocks")
