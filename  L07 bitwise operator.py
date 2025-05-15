1c5305399516ec1a700f104771c2803c4c909825
Prog-Smiler <progsmiler@gmail.com>
2025-05-15 20:08:39 +0545

    L07

---
+++  L07 bitwise operator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+print("a >> 1 = ", a >> 1)
+print("b >> 1 = ", b >> 1)
+
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+
+print("a << 1 = ",  a << 1)
+print("b << 1 = ", b << 1)
